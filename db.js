6abbfe5d7db54650b29e9ffb2bd04a460304c7da
Kang Taehoon <jadetypehoon@gmail.com>
2019-09-01 14:44:54 +0900

    implement model-user it will be place "model" directory

--- db.js
+++ db.js
@@ -1,1 +1,22 @@
-const sequelize = require ('sequelize')
+const Sequelize = require ('sequelize');
+const secret = require ('config/secret');
+
+const sequelize = new Sequelize(
+    global.gConfig.database,
+    secret.DB_ID,
+    secret.DB_PASSWORD,
+    {
+      'host': secret.DB_HOST,
+      'dialect': 'mysql'
+    }
+);
+sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err);
+    });
+
+export default sequelize;
---
+++ model-user.js
@@ -0,0 +1,44 @@
+const Sequelize = require ('sequelize');
+const sequelize = require('./db');
+const crypto = require('crypto');
+
+module.exports.users = sequelize.define('user',{
+  id: {
+    type:Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email:{
+    type:Sequelize.STRING,
+    allowNULL:false,
+  },
+  password:{
+    type:Sequelize.STRING,
+    allowNULL:false,
+  },
+  nickname:{
+    type:Sequelize.STRING,
+    allowNULL:false,
+  },
+}, {
+      hooks: {
+        // eslint-disable-next-line no-unused-vars
+        afterValidate: (data, options) => {
+          var shasum = crypto.createHash('sha1');
+          shasum.update(data.password);
+          data.password = shasum.digest('hex');
+        }
+      }
+    }
+);
+
+// below SQL 📦 is define data base
+
+// DROP TABLE IF EXISTS `user`;
+// CREATE TABLE `user` (
+//     `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+//     `email` VARCHAR NULL DEFAULT NULL,
+//     `password` VARCHAR NULL DEFAULT NULL,
+//     `nickname` VARCHAR NULL DEFAULT NULL,
+//     PRIMARY KEY (`id`)
+// );
