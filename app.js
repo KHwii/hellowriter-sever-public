d29b5ceb21982a4126e84e40697604b58e88ad80
Kang Taehoon <jadetypehoon@gmail.com>
2019-09-03 23:33:51 +0900

    Change CORS policy

--- app.js
+++ app.js
@@ -17,8 +17,13 @@ const User = require("./db/user");
 
 app.use(express.json());
 app.set("port", global.gConfig.node_port);
-app.use(cors({ credentials: true, origin: "http://localhost:5000" }));
-app.options("*", cors());
+app.use(cors({ credentials: true, origin: "*" }));
+// app.options("/*", function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//   res.send(200);
+// });
 
 app.listen(app.get("port"));
 console.log("Listening on", app.get("port"));
