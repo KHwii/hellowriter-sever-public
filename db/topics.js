ab773bf964c7eab125793865036cbd1fdb2c226b
육광휘 <khwi@yuggwanghwiui-MacBookPro.local>
2019-09-10 13:32:45 +0900

    delete association code more

--- db/topics.js
+++ db/topics.js
@@ -2,35 +2,24 @@ const Sequelize = require("sequelize");
 const sequelize = require("./db");
 const Users = require("./user");
 
-const Topics = sequelize.define(
-  "topics",
-  {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    user_id: {
-      type: Sequelize.INTEGER,
-      allowNULL: false,
-    },
-    topic_text: {
-      type: Sequelize.STRING,
-      allowNULL: false
-    },
-    publish_allow: {
-      type: Sequelize.INTEGER,
-      allowNULL: false
-    }
+const Topics = sequelize.define("topics", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    charset: "utf8",
-    collate: "utf8_unicode_ci",
-    underscored: true,
-    freezeTableName: true,
-    tableName: "topics",
-    timestamps: false
+  user_id: {
+    type: Sequelize.INTEGER,
+    allowNULL: false
+  },
+  topic_text: {
+    type: Sequelize.STRING,
+    allowNULL: false
+  },
+  publish_allow: {
+    type: Sequelize.INTEGER,
+    allowNULL: false
   }
-);
+});
 Topics.sync();
 module.exports.Topics = Topics;
